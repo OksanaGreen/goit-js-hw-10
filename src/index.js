77b4ebb307d09dc5c1e65654385de2c96ac7cd96
OksanaGreen <Springocsana@ukr.net>
2023-06-03 21:14:41 +0300

    ввв

--- src/index.js
+++ src/index.js
@@ -7,34 +7,33 @@ new SlimSelect({
 const catSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 // 1.HTTP - запити( і повертає проміс із масивом порід - результатом запиту. )
-function getCat(breedsName, breedId) {
-  // https://api.thecatapi.com/v1/images/search?api_key=live_sHTcMzdhlueqqIpYr2EasVgaHY3pGwpKXvGupiU3EuYOITKGy0u5HY4qZgnVuIUr&limit=100&has_breeds=0&breed_ids=none
-  const BaseUrl = 'https://api.thecatapi.com/v1/images/search';
+let breedId;
+function fetchBreeds() {
+  const BaseUrl = 'https://api.thecatapi.com/v1/';
   const ApiKey =
     'live_sHTcMzdhlueqqIpYr2EasVgaHY3pGwpKXvGupiU3EuYOITKGy0u5HY4qZgnVuIUr';
 
-  return fetch(
-    `${BaseUrl}?api_key=${ApiKey}&limit=100&has_breeds${breedsName}&breed_ids${breedId}`
-  )
+  const r = fetch(`${BaseUrl}breeds?api_key=${ApiKey}`);
+  console
+    .log(r)
     .then(resp => {
-      return resp.joson();
+      if (!resp.ok) {
+        throw new Error(resp.statusText, 'p.error');
+      }
+
+      return resp.json();
     })
-    .then((has_breeds, breed_ids) => {
-      console.log(has_breeds, breed_ids);
-    });
+    .catch(err => console.log(err));
 }
-//   if (!resp.ok) {
-//     throw new Error(resp.statusText, 'p.error');
-//   }
-//   return resp.jeson();
+// fetchBreeds().then(data => {
+//   console.log(data);
 // });
 
-getCat();
-// fetchBreeds()
 //   .then(data => console.log(data))
-//   .catch(err => console.log(err));
+//
 // // 2.Колекція порід
 // // 3.Інформація про кота
 // fetchCatByBreed(breedId);
 // // 4.Опрацювання стану завантаження
 // // 5.Опрацювання помилки
+// https://api.thecatapi.com/v1/images/search?breed_ids={breed.id}
