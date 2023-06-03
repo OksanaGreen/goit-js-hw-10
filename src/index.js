8645ce1183372b55d9f407da28a8dd072e9ddf69
OksanaGreen <Springocsana@ukr.net>
2023-06-03 13:33:35 +0300

    tttt

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'slim-select/dist/slimselect.css';
 new SlimSelect({
   select: '#selectElement',
 });
+const catSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
 // 1.HTTP - запити( і повертає проміс із масивом порід - результатом запиту. )
 function getCat(breedsName, breedId) {
   // https://api.thecatapi.com/v1/images/search?api_key=live_sHTcMzdhlueqqIpYr2EasVgaHY3pGwpKXvGupiU3EuYOITKGy0u5HY4qZgnVuIUr&limit=100&has_breeds=0&breed_ids=none
@@ -13,9 +15,13 @@ function getCat(breedsName, breedId) {
 
   return fetch(
     `${BaseUrl}?api_key=${ApiKey}&limit=100&has_breeds${breedsName}&breed_ids${breedId}`
-  );
-
-  then(resp => console.log(resp));
+  )
+    .then(resp => {
+      return resp.joson();
+    })
+    .then((has_breeds, breed_ids) => {
+      console.log(has_breeds, breed_ids);
+    });
 }
 //   if (!resp.ok) {
 //     throw new Error(resp.statusText, 'p.error');
